fa22db2a8160ab7bbaa8885bf5a597097c1e9c55
Matthew Posey <mparkerposey@outlook.com>
2024-02-15 14:15:28 -0600

    bug walking animation visible and scaled up

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+let bugs = [];
+
+class Bug
+{
+  constructor(sheetPath, xPos, yPos)
+  {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.sprite = new Sprite(xPos, yPos, 32, 32);
+    this.sprite.spriteSheet = sheetPath;
+    this.sprite.anis.frameDelay = 6;
+    this.animations = {
+      walkLeft: {row: 0, frames: 8},
+      squished: {row: 1, frames: 1}
+    };
+    this.sprite.addAnis(this.animations);
+    this.sprite.changeAni('walkLeft');
+  }
+}
+
+function preload()
+{
+    let newBug = new Bug('assets/BugAnimations.png', 100, 100);
+    bugs.unshift(newBug);
+
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
+  clear();
   background(220);
+
+  //animations are drawn last
+  scale(2);
 }
